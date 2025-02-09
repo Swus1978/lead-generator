9aab220f2ff6b6bdd641110bd9576be3732ee94b
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-02-09 16:36:07 -0500

    updated code

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/11.3.0/firebasedatabase.js";
-
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js"
+import { getDatabase, 
+         ref, 
+         push, 
+         onValue, 
+         remove 
+        } from "https://www.gstatic.com/firebasejs/11.3.0/firebasedatabase.js"
 
 const firebaseConfig = {
-    databaseURL: ProcessingInstruction.env.DATABASE_URL
-
+    databaseURL:  "https://www.gstatic.com/leads-tracker-app-3101d.firebasestorage.app"
 }
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
 
-console.log(firebaseConfig.databaseURL)
 
-let myLeads = []
+
+
+
+
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
-const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-const tabBtn = document.getElementById("tab-btn")
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
-
-tabBtn.addEventListener("click", function(){    
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        myLeads.push(tabs[0].url)
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 
 function render(leads) {
     let listItems = ""
@@ -48,15 +38,26 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
+onValue(referenceInDB, function(snapshot){
+
+    const snapshotDoesExist = snapshot.exists()
+
+    if (snapshotDoesExist) {
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+
+    }
+})
+
+
+
 deleteBtn.addEventListener("dblclick", function() {
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
 })
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-    render(myLeads)
 })
